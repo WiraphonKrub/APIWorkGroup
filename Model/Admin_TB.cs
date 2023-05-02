052006ca4034af40573c81e3a609474da49c2f01
wiraphon <wiraphon.597@gmail.com>
2023-05-02 08:42:13 +0700

    first commit

---
+++ Model/Admin_TB.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace APIStore.Model
+{
+
+    public class Admin_TB
+    {
+
+        // -------------- Primary Key ----------------
+
+        [Key]
+        public int Admin_id { get; set; }
+
+        // -------------- Primary Key ----------------
+
+        public string? Admin_name { get; set; }
+        public string? Admin_username { get; set; }
+        public string? Admin_password { get; set; }
+        public string? Admin_email { get; set; }
+        public string? Admin_address { get; set; }
+
+        // -------------- Foreign Key ----------------
+
+        public int Admin_typeid { get; set; }
+        public int Admin_statusid { get; set; }
+
+        // -------------- Foreign Key ----------------
+    }
+}
---
+++ Model/DataContext.cs
@@ -0,0 +1,25 @@
+namespace APIStore.Model;
+
+using Microsoft.EntityFrameworkCore;
+using APIStore.Model;
+
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to mysql with connection string from app settings
+        var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
+
+    public DbSet<Admin_TB> admin_TBs { get; set; }
+}
+
